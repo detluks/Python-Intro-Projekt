bf8fd26902b634742e217f59ece7b152011f4f06
detluks <lukas@mirweb.dk>
2025-08-25 08:48:10 +0200

    alienSpawn+newBulletSound

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import pygame 
+import random
 
 
 pygame.init()
@@ -7,24 +8,36 @@ width, height = 500,750
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Spaceshooter")
 player=[]
+alienImg=[]
 for i in range(3):
     img= pygame.image.load(f"images/ship_{i}.png")
     player.append(img)
+for i in range(2):
+    img = pygame.image.load(f"images/alien_{i}.png")
+    alienImg.append(img)
 bulletSound = pygame.mixer.Sound("sounds/laser.mp3")
 speed = 8
 bulletSpeed=10
 bullets=[]
+aliens=[]
+framesTimer=0
+alienSpeed=6
 bulletFired=False
 x,y=width/2,height*(2/3)
 player_width=player[0].get_rect().size[0]
+alien_width=alienImg[0].get_rect().size[0]
+alien_Height=alienImg[0].get_rect().size[1]
 clock = pygame.time.Clock()
 frame=0
+a = 0
 running = True
 w_pressed=False
 s_pressed=False
 a_pressed=False
 d_pressed=False
-class bullet:
+
+
+class Bullet:
     def __init__(self,x,y,speed):
         self.x=x
         self.y=y
@@ -34,6 +47,17 @@ class bullet:
     def draw(self):
         pygame.draw.rect(screen, (255, 0, 0), (self.x, self.y, 5, 10))
 
+class Alien:
+    def __init__(self,x,y,speed):
+        self.x=x
+        self.y=y
+        self.speed=speed
+    def move(self):
+        self.y+=self.speed
+    def draw(self):
+        screen.blit(alienImg[a],(self.x,self.y))
+
+    
 while running:
     screen.fill((100,100,100))
     for event in pygame.event.get():
@@ -80,15 +104,22 @@ while running:
         x-=speed
     if bulletFired:
         bulletSound.play()
-        bullets.append(bullet(x+player_width/2,y,bulletSpeed))
+        bullets.append(Bullet(x+player_width/2,y,bulletSpeed))
         bulletFired=False
 
     for element in bullets:
         element.move()
         element.draw()
+        if element.y<=0:
+            bullets.remove(element)
         
-
-    
+    if framesTimer>200:
+        aliens.append(Alien(random.randint(0,width-alien_width),0,alienSpeed))
+        framesTimer = 0
+    a=int(frame/4)%2
+    for element in aliens:
+        element.move()
+        element.draw()
 
 
 
@@ -98,6 +129,7 @@ while running:
     pygame.display.flip()
     clock.tick(60)
     frame+=1
+    framesTimer+=1
     
 
 pygame.quit()
